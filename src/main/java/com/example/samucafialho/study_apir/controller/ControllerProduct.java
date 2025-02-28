5b721ca5e5ad4571f567e5a7810a295e180be9f4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 22:52:51 -0300

    feat: Services-create-delete-get-update

--- src/main/java/com/example/samucafialho/study_apir/controller/ControllerProduct.java
+++ src/main/java/com/example/samucafialho/study_apir/controller/ControllerProduct.java
@@ -1,5 +1,6 @@
 package com.example.samucafialho.study_apir.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,28 +9,37 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.samucafialho.study_apir.service.ProductService;
+
 @RestController
 @RequestMapping("produtos")
 public class ControllerProduct {
 
+    @Autowired
+    private ProductService productservice = new ProductService();
+
     @PostMapping
     public ResponseEntity<String> create() {
+        productservice.createProduct(null);
         return ResponseEntity.status(201).body("Produto cadastrado");
     }
 
     @DeleteMapping
     public ResponseEntity<Void> delete() {
+        productservice.deleteProduct(0);
         return ResponseEntity.status(204).build();
 
     }
 
     @PutMapping
     public ResponseEntity<String> update() {
+        productservice.updateProduct(0, null);
         return ResponseEntity.status(200).body("Produto atualizado");
     }
 
     @GetMapping
     public ResponseEntity<String> find() {
+        productservice.getProductById(0);
         return ResponseEntity.status(200).body("Maça");
 
     }
--- src/main/java/com/example/samucafialho/study_apir/service/ProductService.java
+++ src/main/java/com/example/samucafialho/study_apir/service/ProductService.java
@@ -2,10 +2,28 @@ package com.example.samucafialho.study_apir.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.example.samucafialho.study_apir.model.Product;
 
+@Service
 public class ProductService {
-
     private List<Product> products;
 
+    public Product createProduct(Product product){
+
+        return null;
+    }
+    public Product getProductById(long id){
+
+        return null;
+    }
+    public Product updateProduct(long id, Product product){
+
+        return null;
+    }
+    public void deleteProduct(long id){
+
+
+    }
 }
