78146f11eb4ebae9deddaa02db7d943523dcd4ce
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-06 22:55:24 -0300

    ultima aula

--- src/main/java/com/example/samucafialho/study_apir/controller/ControllerProduct.java
+++ src/main/java/com/example/samucafialho/study_apir/controller/ControllerProduct.java
@@ -1,21 +1,36 @@
 package com.example.samucafialho.study_apir.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.samucafialho.study_apir.model.Product;
+import com.example.samucafialho.study_apir.service.ProductService;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("produtos")
 public class ControllerProduct {
 
+    @Autowired
+    private ProductService service;
+
     @PostMapping
-    public ResponseEntity<String> create() {
-        return ResponseEntity.status(201).body("Produto cadastrado");
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        //System.out.println(product.getNome());
+        Product productCreated = service.createProduct(product);
+        return ResponseEntity.status(201).body(productCreated);
+
     }
+
 
     @DeleteMapping
     public ResponseEntity<Void> delete() {
@@ -23,14 +38,15 @@ public class ControllerProduct {
 
     }
 
-    @PutMapping
-    public ResponseEntity<String> update() {
-        return ResponseEntity.status(200).body("Produto atualizado");
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+        Product productUpdated = service.updateProduct(id, product);
+        return ResponseEntity.status(200).body(productUpdated);
     }
 
     @GetMapping
     public ResponseEntity<String> find() {
-        return ResponseEntity.status(200).body("Maça");
+        return ResponseEntity.status(200).body("mamao");
 
     }
 }
--- src/main/java/com/example/samucafialho/study_apir/service/ProductService.java
+++ src/main/java/com/example/samucafialho/study_apir/service/ProductService.java
@@ -1,11 +1,37 @@
 package com.example.samucafialho.study_apir.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
 
 import com.example.samucafialho.study_apir.model.Product;
 
+@Service
 public class ProductService {
 
-    private List<Product> products;
+    private List<Product> products = new ArrayList<>();
+    private Long sequence = 1L;
+    public Product createProduct(Product product){
+        product.setId(sequence++);
+        products.add(product);
+
+        return product;
+    }
+    public Product getProductbyId(Long id){
+        return null;
+    }
+    public Optional<Product> updateProduct(Long id, Product product){
+
+       return products.stream().filter(e -> e.getId().equals(id)).findFirst().map(p -> {
+        p.setNome(product.getNome());
+        return p;
+       });
+    }
+    public Product deleteProduct(Long id){
+        return null;
+    }
+
 
 }
