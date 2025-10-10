a8e98f906218047e16965657a87ae1d8166e0fec
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-09 22:14:56 -0300

    feat: pongx-

--- src/main/java/com/example/samucafialho/study_apir/controller/ControllerPing.java
+++ src/main/java/com/example/samucafialho/study_apir/controller/ControllerPing.java
@@ -15,7 +15,7 @@ public class ControllerPing {
 
     @GetMapping
     public String ping() {
-        return "pongx";
+        return "pongx-";
     }
     @GetMapping("/version")
     public String version() {
